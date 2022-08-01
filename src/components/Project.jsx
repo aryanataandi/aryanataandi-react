2126254e311200406fc9a3e426f622bd19628e82
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-08-02 00:24:08 +0700

    updated project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,61 +1,104 @@
 import React from 'react';
 import parse from 'html-react-parser';
 import komodoImage from '../assets/project-komodo.png';
+import skbImage from '../assets/project-skb.png';
 
 const Project = () => {
   const projects = [
     {
       id: 1,
+      link: 'belanja.skb.salatiga.go.id',
+      title: 'SKB Online Shop',
+      img: skbImage,
+      desc: `
+      Web based B2C <i>e-commerce</i> built for Salatiga City's SMEs to support their business.
+      This is my first application project that was built back in the day when I was doing an internship.
+      `,
+      tech: ['PHP', 'CodeIgniter', 'MySQL', 'jQuery'],
+    },
+    {
+      id: 2,
       link: 'komodo-os.my.id',
       title: 'Komodo OS Website',
       img: komodoImage,
       desc: `
-      This is a simple website to make it easier for users to know more about Komodo OS.
-      Built with minimalist & clean design using various custom styling to make it responsive.
-      It's also provides data for users to be able to download files provided by the Komodo OS team.
+      A simple website about one of the most popular custom AOSP in Indonesia, Komodo OS.
+      Provides information & latest releases of many devices by the Komodo OS team.
       `,
-      tech: ['Sass', 'Vue', 'Bootstrap'],
-    },
-    {
-      id: 2,
-      link: 'simpus.salatiga.go.id',
-      title: 'Management System',
-      img: komodoImage,
-      desc: `
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-      Et ut, asperiores ea quas eos consequuntur natus laudantium magni? Exercitationem dolor et adipisci harum. 
-      Repellat fugit a nesciunt dicta adipisci veniam sint, qui sequi ducimus, accusamus culpa, doloribus aut nisi quos?
-      `,
-      tech: [],
+      tech: ['SCSS', 'Vue', 'Bootstrap'],
     },
   ];
 
-  return (
-    <>
-      {projects.map((project) => (
+  const LayoutOdd = (props) => {
+    return (
+      <>
         <div className="row py-3 py-lg-5">
           <div className="col-6 d-none d-lg-flex">
-            <img src={project.img} alt={project.title} className="my-auto rounded-3" style={{ width: '110%' }} />
+            <img src={props.img} alt={props.title} className="my-auto rounded-3" style={{ width: '110%' }} />
           </div>
           <div className="col-12 col-lg-6 d-flex text-lg-end">
-            <div className="projects my-auto">
-              <a href={`https://${project.link}/`} className="text-decoration-none" target="_blank">
-                {project.link} &rarr;
+            <div className="props my-auto">
+              <a href={`https://${props.link}/`} className="text-decoration-none" target="_blank">
+                {props.link} &rarr;
               </a>
-              <h1 className="display-6">{project.title}</h1>
-              <div className="card shadow rounded-3 bg-dark my-3">
+              <h1 className="display-6">{props.title}</h1>
+              <div className="card border-0 shadow rounded-3 bg-dark my-3">
                 <div className="card-body">
-                  <p className="fs-6 mb-0">{parse(project.desc)}</p>
+                  <p className="fs-6 mb-0">{parse(props.desc)}</p>
                 </div>
               </div>
               <div className="">
-                {project.tech.map((item) => (
+                {props.tech.map((item) => (
                   <span className="text-primary fw-bold ms-0 ms-lg-4 me-4 me-lg-0">{item}</span>
                 ))}
               </div>
             </div>
           </div>
         </div>
+      </>
+    );
+  };
+
+  const LayoutEven = (props) => {
+    return (
+      <>
+        <div className="row py-3 py-lg-5">
+          <div className="col-12 col-lg-6 d-flex">
+            <div className="props my-auto">
+              <a href={`https://${props.link}/`} className="text-decoration-none" target="_blank">
+                {props.link} &rarr;
+              </a>
+              <h1 className="display-6">{props.title}</h1>
+              <div className="card border-0 shadow rounded-3 bg-dark my-3">
+                <div className="card-body">
+                  <p className="fs-6 mb-0">{parse(props.desc)}</p>
+                </div>
+              </div>
+              <div className="">
+                {props.tech.map((item) => (
+                  <span className="text-primary fw-bold me-4">{item}</span>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="col-6 d-none d-lg-inline">
+            <img src={props.img} alt={props.title} className="my-auto rounded-3 float-end" style={{ width: '110%' }} />
+          </div>
+        </div>
+      </>
+    );
+  };
+
+  return (
+    <>
+      {projects.map((project) => (
+        <>
+          {project.id % 2 !== 0 ? (
+            <LayoutOdd id={project.id} link={project.link} title={project.title} img={project.img} desc={project.desc} tech={project.tech} />
+          ) : (
+            <LayoutEven id={project.id} link={project.link} title={project.title} img={project.img} desc={project.desc} tech={project.tech} />
+          )}
+        </>
       ))}
     </>
   );
