7643174025c4f18fbfce2ec100a15835687f78ce
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-08-01 07:10:52 +0700

    update Project comp

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import parse from 'html-react-parser';
 import komodoImage from '../assets/project-komodo.png';
 
 const Project = () => {
@@ -6,24 +7,26 @@ const Project = () => {
     {
       id: 1,
       link: 'komodo-os.my.id',
-      title: 'Komodo OS Official Website',
+      title: 'Komodo OS Website',
+      img: komodoImage,
       desc: `
-      This is a simple website to make it easier for users to know more about <span>Tesat<span/>Komodo OS.
-      Built with minimalist & clean design. Using various custom styling to make it responsive. 
+      This is a simple website to make it easier for users to know more about Komodo OS.
+      Built with minimalist & clean design using various custom styling to make it responsive.
       It's also provides data for users to be able to download files provided by the Komodo OS team.
       `,
-      img: komodoImage,
+      tech: ['Sass', 'Vue', 'Bootstrap'],
     },
     {
       id: 2,
       link: 'simpus.salatiga.go.id',
       title: 'Management System',
+      img: komodoImage,
       desc: `
       Lorem ipsum dolor sit amet consectetur adipisicing elit. 
       Et ut, asperiores ea quas eos consequuntur natus laudantium magni? Exercitationem dolor et adipisci harum. 
       Repellat fugit a nesciunt dicta adipisci veniam sint, qui sequi ducimus, accusamus culpa, doloribus aut nisi quos?
       `,
-      img: komodoImage,
+      tech: [],
     },
   ];
 
@@ -32,18 +35,23 @@ const Project = () => {
       {projects.map((project) => (
         <div className="row py-3 py-lg-5">
           <div className="col-6 d-none d-lg-flex">
-            <img src={project.img} alt={project.title} className="my-auto rounded-3" style={{width: '110%'}}/>
+            <img src={project.img} alt={project.title} className="my-auto rounded-3" style={{ width: '110%' }} />
           </div>
           <div className="col-12 col-lg-6 d-flex text-lg-end">
             <div className="projects my-auto">
               <a href={`https://${project.link}/`} className="text-decoration-none" target="_blank">
                 {project.link} &rarr;
               </a>
-              <h2>{project.title}</h2>
-              <div className="card shadow-lg rounded-3 bg-dark">
+              <h1 className="display-6">{project.title}</h1>
+              <div className="card shadow rounded-3 bg-dark my-3">
                 <div className="card-body">
-                  <p className="fs-6">{project.desc}</p>
+                  <p className="fs-6 mb-0">{parse(project.desc)}</p>
                 </div>
+              </div>
+              <div className="">
+                {project.tech.map((item) => (
+                  <span className="text-primary fw-bold ms-0 ms-lg-4 me-4 me-lg-0">{item}</span>
+                ))}
               </div>
             </div>
           </div>
