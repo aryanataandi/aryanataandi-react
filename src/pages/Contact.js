c1945d99ddb5cbbfc52719529ae374aecf3e4d94
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-08-28 07:25:38 +0700

    update contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import './home.css';
 
 function Contact() {
   return (
     <>
-      <section className="d-flex">
-        <div className="m-auto text-center">
-          <h1 className="display-1">Coming Soon!</h1>
+      <section className="d-flex contact">
+        <div className="my-auto">
+          <p className="fs-2 mb-0 text-primary">Let's talk about it</p>
+          <div>
+            <h1 className="my-3">Aryanata Andipradana</h1>
+            <h2>
+              <a
+                className="text-decoration-none"
+                href="https://wa.me/6285875351528"
+                target="_blank"
+                rel="noreferrer"
+              >
+                +62 8587 5351 528
+              </a>
+              <br />
+              <a href="mailto:aryanata.andipra@gmail.com">
+                aryanata.andipra@gmail.com
+              </a>
+            </h2>
+          </div>
         </div>
       </section>
     </>
