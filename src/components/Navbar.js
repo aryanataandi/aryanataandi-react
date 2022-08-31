8197915f4097ffb95536b087bee3346a8c6982de
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-08-31 23:17:09 +0700

    updated responsive styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,42 @@ import resume from '../resume.pdf';
 
 function Navbar() {
   const [hide, setHide] = useState(false);
+  const [expand, setExpand] = useState(true);
+
   let prevScrollpos = window.pageYOffset;
+
   const controlNavbar = () => {
     let currentScrollPos = window.pageYOffset;
+    let collapseMenu = document.getElementById('navbarMenu');
+    let toggleMenu = document.getElementById('toggleMenu');
+    let collapseTimeOut = () => {
+      setTimeout(() => {
+        collapseMenu.classList.remove('show');
+      }, 300);
+    };
+
     if (prevScrollpos > currentScrollPos) {
       setHide(false);
     } else {
       setHide(true);
+      if (collapseMenu.classList.contains('show')) {
+        collapseTimeOut();
+        toggleMenu.classList.add('collapsed');
+      }
     }
+
     prevScrollpos = currentScrollPos;
+  };
+
+  const navPosition = () => {
+    if (
+      document.body.scrollTop > 70 ||
+      document.documentElement.scrollTop > 70
+    ) {
+      setExpand(false);
+    } else {
+      setExpand(true);
+    }
   };
 
   useEffect(() => {
@@ -25,45 +52,54 @@ function Navbar() {
     };
   }, []);
 
+  useEffect(() => {
+    window.addEventListener('scroll', navPosition);
+
+    return () => {
+      window.removeEventListener('scroll', navPosition);
+    };
+  }, []);
+
   return (
     <nav
-      className={
-        hide
-          ? 'navbar navbar-expand-md py-3'
-          : 'navbar navbar-expand-md py-3 show'
-      }
+      className={`navbar navbar-expand-md
+        ${hide ? '' : 'show'}
+        ${expand ? 'pt-5' : 'py-3'}
+          `}
     >
       <div className="container-fluid my-container">
-        <Link className="navbar-brand" to="/">
+        <a className="navbar-brand" href="/">
           <img src={logo} alt="logo" width="34" className="pb-1" />
-        </Link>
+        </a>
         <button
-          className="navbar-toggler"
+          className="navbar-toggler collapsed"
+          id="toggleMenu"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#navbarText"
+          data-bs-target="#navbarMenu"
         >
-          <i className="fas fa-bars my-1 text-light"></i>
+          <span></span>
+          <span></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarText">
+        <div className="collapse navbar-collapse" id="navbarMenu">
           <span className="navbar-text me-auto"></span>
           <ul className="navbar-nav mb-2 mb-md-0">
-            <li className="nav-item ms-md-4">
+            <li className="nav-item ms-md-4 mt-2 mt-md-0">
               <Link to="/" className="nav-link">
                 Home
               </Link>
             </li>
-            <li className="nav-item ms-md-4">
+            <li className="nav-item ms-md-4 mt-2 mt-md-0">
               <Link to="/about" className="nav-link">
                 About
               </Link>
             </li>
-            <li className="nav-item ms-md-4">
+            <li className="nav-item ms-md-4 mt-2 mt-md-0">
               <Link to="/contact" className="nav-link">
                 Contact
               </Link>
             </li>
-            <li className="nav-item ms-md-4 mt-2 mt-md-0">
+            <li className="nav-item ms-md-4 mt-3 mt-md-0">
               <Link
                 to={resume}
                 className="btn btn-outline-primary text-primary"
