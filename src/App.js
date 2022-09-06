3a7a045b59a8b3f159253429962d46a84bb2690c
aryanataandi <a.ryan.ata.ap@gmail.com>
2022-09-07 04:17:20 +0700

    added scroll to top comp

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import Contact from './pages/Contact';
 import PageNotFound from './pages/PageNotFound';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Router>
+        <ScrollToTop />
         <Navbar />
         <div className="my-container px-2">
           <Routes>
